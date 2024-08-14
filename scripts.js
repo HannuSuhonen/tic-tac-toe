3a2792525ca599215d62e8f1c4210c360f4a348d
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-08-14 11:14:29 +0300

    Create grid and detect grid element clicked

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log("Hello world!")
+const gameboadContainer = document.querySelector(".gameboard");
+const gameboad = (function createGameboard(doc){
+    let gameBoardTiles = []
+    for(let i = 0; i < 9; i++){
+        const gridElement = doc.createElement("div");
+        gridElement.textContent = i;
+        gameboadContainer.appendChild(gridElement);
+        gameBoardTiles.push(gridElement)
+    }
+    return{
+        gameBoardTiles,
+    }
+})(document);
+
+gameboad.gameBoardTiles.forEach((element,index) => {
+    element.addEventListener("click",() => {
+        element.textContent = "Clicked!"
+        console.log(index);
+    })
+});
