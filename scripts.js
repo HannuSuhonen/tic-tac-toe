39f21a77ea828b7e2bb0dfa7e6958c6a6766cb5d
hannu <49480722+HannuSuhonen@users.noreply.github.com>
2024-08-19 18:08:11 +0300

    Create grid in css

--- scripts.js
+++ scripts.js
@@ -1,42 +1,46 @@
-const gameboad = (function createGameboard(){
-    let gameBoardTiles =    [["1","2","13"],
-                            ["1","13","6"],
-                            ["13","6","6"]]
-    return{
-        gameBoardTiles,
-    }
-})();
+const gameboad = (function createGameboard(doc){
+    const gameBoardTiles = [];
+    const gridItems = doc.querySelectorAll(".grid");
+    Array.from(gridItems).forEach((element) => {
+        gameBoardTiles.push(element);
+    })
+    while(gridItems.length){
+        gameBoardTiles.push(gridItems.splice(0,3));
+    } 
+    console.log(gameBoardTiles);
 
-function checkRows(){
-    const { gameBoardTiles } = gameboad;
-    for(let i = 0; i < gameBoardTiles.length; i++){
-        if(checkForThreeMatch(gameBoardTiles[i]) === true){
-            return true;
+    function checkRows() {
+        for(let i = 0; i < gameBoardTiles.length; i++){
+            if(checkForThreeMatch(gameBoardTiles[i]) === true){
+                return true;
+            }
         }
-    }
-    return false;
-}
-
-function checkColumns(){
-    const { gameBoardTiles } = gameboad;
-    for(let i = 0; i < gameBoardTiles.length; i++){
-        let column =  gameBoardTiles.map((arr) => arr[i]);
-        if(checkForThreeMatch(column)) return true;
-    }
-    return false;
-}
-
-function checkDiagonal(){
-    const { gameBoardTiles } = gameboad;
-    const diag1 = gameBoardTiles.map((row, index) => row[index]);
-    const diag2 = gameBoardTiles.reverse().map((row, index) => row[index]);
-    console.log(diag2);
-    return checkForThreeMatch(diag1) || checkForThreeMatch(diag2);
-}
-
-function checkForThreeMatch(arr){
-    if (arr.length === 0 || arr[0] == null || arr[0] === "") {
         return false;
     }
-    return arr.every(element => element === arr[0]);
-}
+
+    function checkColumns() {
+        for(let i = 0; i < gameBoardTiles.length; i++){
+            let column =  gameBoardTiles.map((arr) => arr[i]);
+            if(checkForThreeMatch(column)) return true;
+        }
+        return false;
+    }
+
+    function checkDiagonal() {
+        const diag1 = gameBoardTiles.map((row, index) => row[index]);
+        const diag2 = gameBoardTiles.reverse().map((row, index) => row[index]);
+        return checkForThreeMatch(diag1) || checkForThreeMatch(diag2);
+    }
+
+    function checkForThreeMatch(arr){
+        if (arr.length === 0 || arr[0] == null || arr[0] === "") {
+            return false;
+        }
+        return arr.every(element => element === arr[0]);
+    }
+
+    return{
+        checkwin : checkRows() || checkColumns() || checkDiagonal(),
+    }
+})(document);
+
